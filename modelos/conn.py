4a681c40b15924ccce9cc635dddac196b64bd7c3
Juan Sebastián Zuluaga León <juanzuluaga@MacBook-Pro-de-Juan.local>
2025-06-02 23:11:00 -0500

    Ajuste de la conexión a la bd

--- modelos/conn.py
+++ modelos/conn.py
@@ -3,10 +3,11 @@ import psycopg2
 def obtener_conexion():
     try:
         conn = psycopg2.connect(
-            host="postgresql://root:CfaSAYkRnGA56sW5YLV4xfAhAuNS3dGh@dpg-d0v66cvfte5s739a9j7g-a.oregon-postgres.render.com/entregascondor_bd",
+            host="dpg-d0v66cvfte5s739a9j7g-a.oregon-postgres.render.com",
             database="entregascondor_bd",
             user="root",
-            password="CfaSAYkRnGA56sW5YLV4xfAhAuNS3dGh"
+            password="CfaSAYkRnGA56sW5YLV4xfAhAuNS3dGh",
+            port=5432
         )
         return conn
     except Exception as e:
