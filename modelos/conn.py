8ca9de76037ff28ab73caeaa17ba6972f0af2f74
Nunito07 <sebastianzzz123456@gmail.com>
2026-03-03 01:06:19 -0500

    Fix SSL connection to Render PostgreSQL

--- modelos/conn.py
+++ modelos/conn.py
@@ -1,15 +1,13 @@
 import psycopg2
+import os
 
 def obtener_conexion():
     try:
         conn = psycopg2.connect(
-            host="dpg-d0v66cvfte5s739a9j7g-a.oregon-postgres.render.com",
-            database="entregascondor_bd",
-            user="root",
-            password="CfaSAYkRnGA56sW5YLV4xfAhAuNS3dGh",
-            port=5432
+            os.environ.get("DATABASE_URL"),
+            sslmode="require"
         )
         return conn
     except Exception as e:
         print("Error al conectar a la base de datos:", e)
-        raise  # vuelve a lanzar el error para que quien llame lo maneje
+        raise
